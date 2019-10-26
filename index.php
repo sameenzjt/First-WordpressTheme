cd76d1e4784701f13cae0b93009db6be0c8c409e
sameenzjt <sameen.zjt@gmail.com>
2019-10-26 11:18:10 +0800

    新增投稿页面

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php get_header(); ?>
 
+<?php
+if ($_SERVER['HTTP_HOST']!='zjt.sameen.art')//如果域名不是adcc.me
+{
+    echo "
+    <script>
+    // 以下方式直接跳转  
+    //飞鸟慕鱼博客
+    window.location.href = 'https://www.sameen.art/';
+   
+</script>
+";}
+?>
+
 <article>
     <div class="lbox">
         <div id="myCarousel" class="carousel slide" data-ride="carousel">
